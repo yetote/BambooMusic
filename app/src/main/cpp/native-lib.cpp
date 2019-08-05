c34ba0507eb51024db712d433d308b8b1f881743
yetote <503779938@qq.com>
2019-08-05 17:23:42 +0800

    修改了点东西...

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,21 @@
 #include <jni.h>
 #include <string>
 
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_yetote_bamboomusic_media_MyPlayer_prepare(JNIEnv *env, jobject thiz, jstring path) {
+    // TODO: implement prepare()
+}extern "C"
+JNIEXPORT void JNICALL
+Java_com_yetote_bamboomusic_media_MyPlayer_play(JNIEnv *env, jobject thiz) {
+    // TODO: implement play()
+}extern "C"
+JNIEXPORT void JNICALL
+Java_com_yetote_bamboomusic_media_MyPlayer_pause(JNIEnv *env, jobject thiz) {
+    // TODO: implement pause()
+}extern "C"
+JNIEXPORT void JNICALL
+Java_com_yetote_bamboomusic_media_MyPlayer_stop(JNIEnv *env, jobject thiz) {
+    // TODO: implement stop()
+}
